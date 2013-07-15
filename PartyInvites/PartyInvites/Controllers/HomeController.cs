e0fdcc6b13ea84ff4caed9d4afa1437688a45dbd
William Getz <willegetz@gmail.com>
2013-07-14 23:37:44 -0700

    End of chapter 2 work

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+	        var hour = DateTime.Now.Hour;
+	        ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+	        return View();
+        }
+
+		[HttpGet]
+		public ViewResult RsvpForm()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ViewResult RsvpForm(GuestResponse guestResponse)
+		{
+			if (ModelState.IsValid)
+			{
+				// TODO: Email response to the party organizer
+				return View("Thanks", guestResponse); 
+			}
+			else
+			{
+				// There is a validate error
+				return View();
+			}
+		}
+    }
+}
